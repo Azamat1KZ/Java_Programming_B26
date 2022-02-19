b95b5b373ca42940b4b79468c8fbd737c03782fb
Azamat1KZ <98862639+Azamat1KZ@users.noreply.github.com>
2022-02-19 10:20:26 -0500

    1

---
+++ src/Donjeta/Don2.java
@@ -0,0 +1,10 @@
+package Donjeta;
+
+public class Don2 {
+    public static void main(String[] args) {
+        int a = 30;
+        byte b = 10;
+        b+= a;
+        System.out.println(b); // 40
+    }
+}
