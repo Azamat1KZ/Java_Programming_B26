613ddd790d60eafdf857e94eba0186adeb2e3c90
Azamat1KZ <98862639+Azamat1KZ@users.noreply.github.com>
2022-03-02 19:54:04 -0500

    1

---
+++ src/day21_loops/Factorial.java
@@ -0,0 +1,15 @@
+package day21_loops;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int a = 5;
+        int result = 1;
+        while (a > 1){
+            System.out.println(result + "*" + a);
+            result *= a;
+            a--;
+        }
+        System.out.println(result);
+    }
+
+}
